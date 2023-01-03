ba1b5373a8e2c46b01bc4f06d1425a11095b72a7
JeremiasTorres030 <98502495+JeremiasTorres030@users.noreply.github.com>
2023-01-03 21:58:16 +0100

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = environ.Env.get_value(env,"SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
